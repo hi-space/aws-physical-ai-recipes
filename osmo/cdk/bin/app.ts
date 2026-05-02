5cee6d239488ec5da181e82ef352002ae4d0fe75
hi-space <hispace.j@gmail.com>
2026-05-02 09:30:33 +0000

    feat(osmo): main stack + app entrypoint (composes all constructs)

---
+++ osmo/cdk/bin/app.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { OsmoStack } from '../lib/osmo-stack';
+
+const app = new cdk.App();
+
+const userId = app.node.tryGetContext('userId') ?? '';
+const region = app.node.tryGetContext('region') ?? process.env.CDK_DEFAULT_REGION;
+const vpcCidr = app.node.tryGetContext('vpcCidr') ?? '10.0.0.0/16';
+const gpuSimMaxNodes = parseInt(app.node.tryGetContext('gpuSimMaxNodes') ?? '8', 10);
+const gpuTrainMaxNodes = parseInt(app.node.tryGetContext('gpuTrainMaxNodes') ?? '4', 10);
+
+if (userId && !/^[a-z0-9-]+$/.test(userId)) {
+  throw new Error(`userId는 영문소문자, 숫자, 하이픈만 허용됩니다: '${userId}'`);
+}
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region,
+};
+
+const userSuffix = userId ? `-${userId}` : '';
+const stackName = `Osmo${userSuffix}`;
+
+new OsmoStack(app, stackName, {
+  env,
+  userId,
+  vpcCidr,
+  gpuSimMaxNodes,
+  gpuTrainMaxNodes,
+});
---
+++ osmo/cdk/lib/osmo-stack.ts
@@ -0,0 +1,76 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NetworkingConstruct } from './constructs/networking';
+import { EksClusterConstruct } from './constructs/eks-cluster';
+import { DataStoresConstruct } from './constructs/data-stores';
+import { OsmoInstallConstruct } from './constructs/osmo-install';
+
+export interface OsmoStackProps extends cdk.StackProps {
+  userId?: string;
+  vpcCidr?: string;
+  gpuSimMaxNodes?: number;
+  gpuTrainMaxNodes?: number;
+}
+
+export class OsmoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: OsmoStackProps) {
+    super(scope, id, props);
+
+    const userId = props.userId ?? '';
+    const userSuffix = userId ? `-${userId}` : '';
+    const namePrefix = `Osmo${userSuffix}`;
+
+    if (userId) {
+      cdk.Tags.of(this).add('UserId', userId);
+    }
+
+    // 1. Networking
+    const networking = new NetworkingConstruct(this, 'Networking', {
+      namePrefix,
+      vpcCidr: props.vpcCidr,
+    });
+
+    // 2. EKS Cluster
+    const eksCluster = new EksClusterConstruct(this, 'EksCluster', {
+      namePrefix,
+      vpc: networking.vpc,
+      privateSubnets: networking.privateSubnets,
+      publicSubnets: networking.publicSubnets,
+      gpuSimMaxNodes: props.gpuSimMaxNodes,
+      gpuTrainMaxNodes: props.gpuTrainMaxNodes,
+    });
+
+    // 3. Data Stores
+    const dataStores = new DataStoresConstruct(this, 'DataStores', {
+      namePrefix,
+      vpc: networking.vpc,
+      privateSubnets: networking.privateSubnets,
+      eksSecurityGroupId: eksCluster.cluster.clusterSecurityGroupId,
+    });
+
+    // 4. OSMO Install
+    new OsmoInstallConstruct(this, 'OsmoInstall', {
+      namePrefix,
+      cluster: eksCluster.cluster,
+      dbEndpoint: dataStores.dbEndpoint,
+      dbPort: dataStores.dbPort,
+      redisEndpoint: dataStores.redisEndpoint,
+      redisPort: dataStores.redisPort,
+      dataBucket: dataStores.bucket,
+    });
+
+    // Stack Outputs
+    new cdk.CfnOutput(this, 'EksClusterName', {
+      value: eksCluster.clusterName,
+      description: 'EKS Cluster Name',
+    });
+    new cdk.CfnOutput(this, 'S3BucketName', {
+      value: dataStores.bucket.ref,
+      description: 'OSMO Data S3 Bucket',
+    });
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: networking.vpc.ref,
+      description: 'VPC ID',
+    });
+  }
+}
